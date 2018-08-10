ba8824a39857e9743ce4156644cdb505e884fe93
martaKopp <kopp.marta@gmail.com>
2018-08-10 10:34:08 +0200

    StudentControllerTests-2 tests(pass dto) are red :(

---
+++ src/test/java/pl/kopp/marta/controller/StudentControllerTest.java
@@ -0,0 +1,96 @@
+package pl.kopp.marta.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.kopp.marta.domain.dto.StudentDto;
+import pl.kopp.marta.domain.service.StudentService;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentControllerTest {
+
+    @Autowired
+    private MockMvc chrome;
+    @Autowired
+    private StudentService service;
+
+    @Test
+    public void create() throws Exception {
+        String name="Peter";
+        String surname="Parker";
+        MockHttpServletResponse response=chrome.perform(MockMvcRequestBuilders.put("/student")
+        .param("name",name)
+        .param("surname",surname)).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        String id = response.getContentAsString();
+        MockHttpServletResponse create = aStudentBy(Long.valueOf(id));
+        assertEquals("{\"name\":\""+name+"\",\"surname\":\"" + surname+"\",\"grade\":null}",create.getContentAsString());
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+        String name="Peter";
+        String surname="Parker";
+        long id=givenStudent(name,surname);
+
+        MockHttpServletResponse response=chrome.perform(MockMvcRequestBuilders.delete("/student/{id}",id))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    public void shouldGet() throws Exception {
+        String name="Peter";
+        String surname="Parker";
+        long id=givenStudent(name,surname);
+
+        MockHttpServletResponse response=aStudentBy(id);
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("{\"name\":\""+name+"\",\"surname\":\"" + surname+"\",\"grade\":null}", response.getContentAsString());
+    }
+
+    private MockHttpServletResponse aStudentBy(long id) throws Exception {
+        return chrome.perform(MockMvcRequestBuilders.get("/student/{id}",id)).andReturn().getResponse();
+    }
+
+    private long givenStudent(String name, String surname) {
+        StudentDto dto=new StudentDto.Builder(name,surname).build();
+        return service.add(dto);
+    }
+
+
+    @Test
+    public void update() throws Exception {
+        String name="Peter";
+        String newName="Spider-man";
+        String surname="Parker";
+        long id=givenStudent(name,surname);
+
+        MockHttpServletResponse response=chrome.perform(MockMvcRequestBuilders
+                .post("/student/{id}")
+        .param("name",newName)
+        .param("surname",surname)
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        MockHttpServletResponse create=aStudentBy(id);
+        assertEquals("{\"name\":\""+newName+"\",\"surname\":\"" + surname+"\",\"grade\":null}", response.getContentAsString());
+    }
+
+}
